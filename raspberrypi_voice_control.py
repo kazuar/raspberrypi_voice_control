5f689daa7c712bc8b6fe8c7f5e821b4869a8e3cf
Tzahi Vidas <tzahi.vidas@gmail.com>
2015-08-14 00:00:12 -0400

    voice control script

--- raspberrypi_voice_control.py
+++ raspberrypi_voice_control.py
@@ -1,22 +1,43 @@
+
 import cec
 import speech_recognition as sr
 
-cec.init()
+TURN_TV_ON = "turn tv on"
+TURN_TV_OFF = "turn tv off"
 
-r = sr.Recognizer()
-while True:
-    with sr.Microphone() as source:
-        print("Recording")
-        audio = r.listen(source)
+def main():
+    # Create cec control
+    cec.init()
 
-    try:
-        command = r.recognize(audio)
-        if "turn on tv" in command.lower():
-            tv = cec.Device(0)
-            tv.power_on()
-        if "turn off tv" in command.lower():
-            tv = cec.Device(0)
-            tv.standby()
-            print("You said " + r.recognize(audio))    # recognize speech using Google Speech Recognition
-    except LookupError:                            # speech is unintelligible
-        print("Could not understand audio")
+    # Ceate speech recognizer object
+    r = sr.Recognizer()
+
+    # Create infinite loop
+    while True:
+        # Record sound
+        with sr.Microphone() as source:
+            print("Recording")
+            audio = r.listen(source)
+
+        try:
+            # Try to recognize the audio
+            command = r.recognize(audio)
+            print("Detected speech:{0}".format(command))
+            # Check the current command
+            if TURN_TV_ON in command.lower():
+                # Get tv device and turn it on
+                tv = cec.Device(0)
+                tv.power_on()
+            elif TURN_TV_OFF in command.lower():
+                # Get tv device and turn it off
+                tv = cec.Device(0)
+                tv.standby()
+        except LookupError:
+            # In case of an exception
+            print("Could not translate audio")
+        except KeyboardInterrupt:
+            # Stop loop
+            print 'Stop voice control'
+
+if __name__ == '__main__':
+    main()
